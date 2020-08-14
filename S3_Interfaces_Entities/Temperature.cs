7308d14c885ed0ca2f4608987bc6f1a2c4f4692c
Jens Zakarias Pedersen <jens7388@edu.campusvejle.dk>
2020-08-14 09:05:42 +0200

    Add project files.

---
+++ S3_Interfaces_Entities/Temperature.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace S3_Interfaces_Entities
+{
+    public class Temperature
+    {
+        public double C { get; set; }
+
+        public double F { get; set; }
+
+        public string Text { get; set; }
+
+        public Temperature(double c, double f, string text)
+        {
+            C = c;
+            F = f;
+            Text = text;
+        }
+    }
+}
