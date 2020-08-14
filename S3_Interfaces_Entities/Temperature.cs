200320279f973317a4681b797df3103e52624d30
Jens Zakarias Pedersen <jens7388@edu.campusvejle.dk>
2020-08-14 09:11:59 +0200

    Added IEquatable interface

--- S3_Interfaces_Entities/Temperature.cs
+++ S3_Interfaces_Entities/Temperature.cs
@@ -2,7 +2,7 @@
 
 namespace S3_Interfaces_Entities
 {
-    public class Temperature
+    public class Temperature: IEquatable<Temperature>
     {
         public double C { get; set; }
 
@@ -15,6 +15,34 @@ namespace S3_Interfaces_Entities
             C = c;
             F = f;
             Text = text;
+        }
+        public bool Equals(Temperature temp)
+        {
+            if(temp == null)
+            {
+                return false;
+            }
+            return C == temp.C &&
+                    F == temp.F &&
+                    string.Equals(Text, temp.Text);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Temperature);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked //makes program ignore over/underflow
+            {
+                //hash codes are multiplied with prime numbers to avoid collisions
+                int hash = 17;
+                hash = hash * 23 + C.GetHashCode();
+                hash = hash * 23 + F.GetHashCode();
+                hash = hash * 23 + Text.GetHashCode();
+                return hash;
+            }
         }
     }
 }
